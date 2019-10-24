ef7b85cba6414fa814b5ef65b139a0051e2bba26
Tomek Piotrowski <tomasz.piotrowski.tp@gmail.com>
2019-10-24 12:50:30 +0200

    Add loop for-of exercise

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 'use strict';
-document.getElementById('test-button').addEventListener('click', function(){
+/*document.getElementById('test-button').addEventListener('click', function(){
     const links = document.querySelectorAll('.titles a');                       // why titles
     console.log('links:', links);
-})
+});
+*/
+const links = document.querySelectorAll('.titles a');
+
+for (let link of links) {
+    console.log(link);
+}
