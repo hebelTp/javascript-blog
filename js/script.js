665099b97d9eddd0d36e8a00c6bf2799d58745c7
Tomek Piotrowski <tomasz.piotrowski.tp@gmail.com>
2019-10-24 10:31:55 +0200

    Add btn and js querySelectorAll

--- js/script.js
+++ js/script.js
@@ -1,1 +1,5 @@
 'use strict';
+document.getElementById('test-button').addEventListener('click', function(){
+    const links = document.querySelectorAll('.titles a');                       // why titles
+    console.log('links:', links);
+})
