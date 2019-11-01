56985530d1e3d3688b0c87a588ec6b775b160f36
Tomek Piotrowski <tomasz.piotrowski.tp@gmail.com>
2019-11-01 11:24:12 +0100

    Add algorithm structure

--- js/script.js
+++ js/script.js
@@ -205,6 +205,63 @@
     }
   }
   generateAuthor();
+
+  const authorClickHandler = function(event) {
+
+    /* prevent default action for this event */
+
+
+    /* make new constant named "clickedElement" and give it the value of "this" */
+
+
+    /* make a new constant "href" and read the attribute "href" of the clicked element */
+
+
+    /* make a new constant "author" and extract author from the "href" constant */
+
+
+    /* find all author links with class active */
+
+
+    /* START LOOP: for each active author link */
+
+
+      /* remove class active */
+
+
+      /* END LOOP: for each active author link */
+
+
+    /* find all author links with "href" attribute equal to the "href" constant */
+
+
+    /* START LOOP: for each found author link */
+
+
+      /* add class active */
+
+
+      /* END LOOP: for each found tag link */
+
+
+    /* execute function "generateAuthorLinks" with article selector as argument */
+    generateAuthorLinks();
+  };
+
+  function addClickListenersToAuthors() {
+    /* find all links to authors */
+
+
+    /* START LOOP: for each link */
+
+
+      /* add authorClickHandler as event listener for that link */
+
+    /* END LOOP: for each link */
+
+  }
+  addClickListenersToAuthors();
+
 }
 
 
